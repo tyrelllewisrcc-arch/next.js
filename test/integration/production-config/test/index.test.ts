9241b9c1452056abf1fbd47364994c88e2eeee8a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-18 23:19:31 +0100

    remove test

--- test/integration/production-config/test/index.test.ts
+++ test/integration/production-config/test/index.test.ts
@@ -1,43 +1,14 @@
 /* eslint-env jest */
 
-import webdriver from 'next-webdriver'
 import { join } from 'path'
-import {
-  nextStart,
-  nextBuild,
-  findPort,
-  killApp,
-  runNextCommand,
-} from 'next-test-utils'
+import { runNextCommand } from 'next-test-utils'
 
 const appDir = join(__dirname, '../')
-
-let app
-let appPort
 
 describe('Production Config Usage', () => {
   ;(process.env.TURBOPACK_DEV ? describe.skip : describe)(
     'production mode',
     () => {
-      beforeAll(async () => {
-        await nextBuild(appDir)
-        appPort = await findPort()
-        app = await nextStart(appDir, appPort)
-      })
-      afterAll(() => killApp(app))
-
-      describe('with generateBuildId', () => {
-        it('should add the custom buildid', async () => {
-          const browser = await webdriver(appPort, '/')
-          const text = await browser.elementByCss('#mounted').text()
-          expect(text).toMatch(/ComponentDidMount executed on client\./)
-
-          const html = await browser.eval('document.documentElement.innerHTML')
-          expect(html).toMatch('custom-buildid')
-          await browser.close()
-        })
-      })
-
       describe('env', () => {
         it('should fail with leading __ in env key', async () => {
           const result = await runNextCommand(['build', appDir], {
